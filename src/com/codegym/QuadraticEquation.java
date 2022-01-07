b974fe8445a368ed785ccf76729609bda1c043dc
lamthuy305 <lamthuy305@gmail.com>
2022-01-07 11:43:24 +0700

    chinh sua

--- src/com/codegym/QuadraticEquation.java
+++ src/com/codegym/QuadraticEquation.java
@@ -1,7 +1,9 @@
 package com.codegym;
 
 public class QuadraticEquation {
-    double a, b, c;
+     private double a;
+     private double b;
+     private double c;
 
     public QuadraticEquation() {
     }
@@ -9,6 +11,30 @@ public class QuadraticEquation {
     public QuadraticEquation(double a, double b, double c) {
         this.a = a;
         this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
         this.c = c;
     }
 
